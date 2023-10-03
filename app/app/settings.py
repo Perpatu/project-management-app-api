51f6cef7a510ba828d1b9cddf32b41af858e84e1
Perpatu <perpatussss@gmail.com>
2023-10-03 20:17:46 +0200

    add nginx

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,14 +21,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$%t0hgc*_yd0#$8%c&a57u*j9l0wbuaiylc(l56^zf^n5$(b*y'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')   #'django-insecure-$%t0hgc*_yd0#$8%c&a57u*j9l0wbuaiylc(l56^zf^n5$(b*y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 
 # Application definition
@@ -136,11 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/static/'
-MEDIA_URL = 'static/media/'
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
 
-MEDIA_ROOT = 'vol/web/media'
-STATIC_ROOT = 'vol/web/static'
+MEDIA_ROOT = '/vol/web/media'
+STATIC_ROOT = '/vol/web/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
